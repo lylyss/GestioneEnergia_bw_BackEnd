1f3d71d8c070d9b9a5430f5861494af1bcc3cc4d
Loboda Nicolae <77647358+lylyss@users.noreply.github.com>
2025-08-06 20:04:43 +0200

    Primo commit

---
+++ epic-energy-frontend-classic/main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const app = document.getElementById("app");
+  app.innerHTML = "<p>Benvenuto nel CRM di EPIC ENERGY SERVICES!</p>";
+});
